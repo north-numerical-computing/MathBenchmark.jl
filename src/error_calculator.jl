d8b2e612139b33cda6ef39a280d17c9c3bafeb49
Mantas Mikaitis <m.mikaitis@leeds.ac.uk>
2026-08-15 14:17:26 +0200

    Reduce MPFR temp object creation in the hot loop

--- src/error_calculator.jl
+++ src/error_calculator.jl
@@ -4,6 +4,9 @@ include("functions.jl")
 using .Functions
 using Printf
 
+using Libdl
+const libmpfr_handle = Libdl.dlopen(:libmpfr)
+
 neg_zeros = Dict{String, Any}()
 neg_zeros["binary16"] = UInt16(0x8000)
 neg_zeros["binary32"] = UInt32(0x80000000)
@@ -27,27 +30,75 @@ subn_mask[Float16] = UInt16(0x03FF)
 subn_mask[Float32] = UInt32(0x007FFFFF)
 subn_mask[Float64] = UInt64(0x000FFFFFFFFFFFFF)
 
+const MPFR_UNARY = Dict(
+    "acos"  => :mpfr_acos,
+    "acosh" => :mpfr_acosh,
+    "asin"  => :mpfr_asin,
+    "asinh" => :mpfr_asinh,
+    "atan"  => :mpfr_atan,
+    "atanh" => :mpfr_atanh,
+    "cbrt"  => :mpfr_cbrt,
+    "cos"   => :mpfr_cos,
+    "cosh"  => :mpfr_cosh,
+    "exp"   => :mpfr_exp,
+    "exp10" => :mpfr_exp10,
+    "exp2"  => :mpfr_exp2,
+    "log"   => :mpfr_log,
+    "log10" => :mpfr_log10,
+    "log1p" => :mpfr_log1p,
+    "log2"  => :mpfr_log2,
+    "sin"   => :mpfr_sin,
+    "sinh"  => :mpfr_sinh,
+    "sqrt"  => :mpfr_sqrt,
+    "tan"   => :mpfr_tan,
+    "tanh"  => :mpfr_tanh,
+    "cospi" => :mpfr_cospi,
+    "sinpi" => :mpfr_sinpi,
+    "tanpi" => :mpfr_tanpi,
+)
+
+function mpfr_fptr(name::Symbol)
+    return Libdl.dlsym(libmpfr_handle, name)
+end
+
 """
 Calculate the error in ulps between a floating-point number y
 and a BigFloat number z, and return it as a BigFloat number.
 
 """
-function get_ulp_error(y::Union{Float16, Float32, Float64}, z::BigFloat)
+function get_ulp_error(y::Union{Float16, Float32, Float64}, z::BigFloat, scratch::BigFloat)
 
-    d = abs(y - z)
+    rnd = Base.MPFR.ROUNDING_MODE[]
+
     wrap = typeof(y)
     rn = convert(wrap, z)
     ulp = eps(rn)
 
+    ccall((:mpfr_abs, :libmpfr), Int32,
+          (Ref{BigFloat}, Ref{BigFloat}, Int32),
+          scratch, z, rnd)
+
     # Reduce ulp if a power of two was reached by rounding up.
-    if abs(rn) > abs(z) && (reinterpret(Err.uint_formats[wrap], rn) &
+    if abs(rn) > scratch && (reinterpret(Err.uint_formats[wrap], rn) &
         Err.subn_mask[wrap] == 0) && abs(rn) != floatmin(wrap)
         ulp = ulp/2
     end
 
-    return d / ulp
-end
+    ccall((:mpfr_sub_d, :libmpfr), Int32,
+          (Ref{BigFloat}, Ref{BigFloat}, Cdouble, Int32),
+          scratch, z, Float64(y), rnd)
+    ccall((:mpfr_abs, :libmpfr), Int32,
+          (Ref{BigFloat}, Ref{BigFloat}, Int32),
+          scratch, scratch, rnd)
 
+    k = -exponent(ulp)
+
+    ccall((:mpfr_mul_2si, :libmpfr), Int32,
+          (Ref{BigFloat}, Ref{BigFloat}, Clong, Int32),
+          scratch, scratch, k, rnd)
+
+    return scratch
+end
 
 """
 Calculate how many floating-point values are in the provided range, inclusive.
@@ -98,6 +149,19 @@ function nextfloatn(x, n, format)
 
 end
 
+function set!(z::BigFloat, x::AbstractFloat)
+    ccall((:mpfr_set_d, :libmpfr), Int32,
+          (Ref{BigFloat}, Cdouble, Int32),
+          z, Float64(x), Base.MPFR.ROUNDING_MODE[])
+    return z
+end
+
+function apply!(z::BigFloat, fptr::Ptr{Cvoid}, x::BigFloat)
+    ccall(fptr, Int32,
+          (Ref{BigFloat}, Ref{BigFloat}, Int32),
+          z, x, Base.MPFR.ROUNDING_MODE[])
+    return z
+end
 
 """
 Given an input value x in one of the three floating-point formats,
@@ -105,8 +169,7 @@ calculate y, the approximation of the function for that format, and
 z, the high precision correctly rounded variant in BigFloat (MPFR).
 
 """
-function calculate_function(x, func, rounding, fastmath_on)
-
+function calculate_function(x, func, apply_fn, rounding, fastmath_on, bigx::BigFloat, bigz::BigFloat, scratch::BigFloat)
     # Note: here the rounding mode could be changed before calling func, but
     # Julia currently does not provide separate mathematical functions with
     # different rounding modes.
@@ -115,21 +178,19 @@ function calculate_function(x, func, rounding, fastmath_on)
     else
         y = func(x)
     end
+
     if isinf(y)
         @warn "The Julia mathematical function has produced infinity:\
                $func. Skipping input $x."
         return (BigFloat(0.0), y, BigFloat(NaN))
     end
 
-    bigx = BigFloat(x)
-    z = func(bigx)
+    set!(bigx, x)
+    apply_fn(bigz, bigx)
 
-    # Calculate the error in ulps.
-    error = get_ulp_error(y, z)
-
-    return (error, y, z)
+    error = get_ulp_error(y, bigz, scratch)
+    return (error, y, bigz)
 end
-
 
 """
 Go through every floating-point value in the provided range and evaluate
@@ -150,9 +211,18 @@ function function_max_error_exhaustive(
 
     x = start_float
 
+    bigx = BigFloat(0.0)
+    bigz = BigFloat(0.0)
+    scratch = BigFloat(0.0)
+
+    fptr = mpfr_fptr(MPFR_UNARY[func])
+    apply_fn = (dest, x) -> apply!(dest, fptr, x)
+
     f = getfield(Base.Math, Symbol(func));
     while x <= end_float
-        (error, y, z) = calculate_function(x, f, rounding, fastmath_on)
+        (error, y, z) = calculate_function(x, f, apply_fn, rounding,
+                                           fastmath_on, bigx, bigz, scratch)
+
         number_of_tests = number_of_tests + 1
         if (isnan(z))
             number_of_infs = number_of_infs + 1
@@ -160,10 +230,10 @@ function function_max_error_exhaustive(
 
         # Update max error and corresponding values.
         if error > max_error
-            max_error = error
+            max_error = deepcopy(error)
             max_input = x
             max_output = y
-            max_ref_out = z
+            max_ref_out = deepcopy(bigz)
         end
 
         x = nextfloat(x);
@@ -195,9 +265,17 @@ function function_max_error_fixed_step(
 
     step_size = ceil(number_of_floats_in_interval(x, end_float, format)/tests_to_do);
 
+    bigx = BigFloat(0.0)
+    bigz = BigFloat(0.0)
+    scratch = BigFloat(0.0)
+
+    fptr = mpfr_fptr(MPFR_UNARY[func])
+    apply_fn = (dest, x) -> apply!(dest, fptr, x)
+
     f = getfield(Base.Math, Symbol(func))
     while x <= end_float
-        (error, y, z) = calculate_function(x, f, rounding, fastmath_on)
+        (error, y, z) = calculate_function(x, f, apply_fn, rounding,
+                                           fastmath_on, bigx, bigz, scratch)
 
         if (isnan(z))
             number_of_infs = number_of_infs + 1
@@ -205,10 +283,10 @@ function function_max_error_fixed_step(
 
         # Update max error and corresponding values.
         if error > max_error
-            max_error = error
+            max_error = deepcopy(error)
             max_input = x
             max_output = y
-            max_ref_out = z
+            max_ref_out = deepcopy(z)
         end
 
         x = nextfloatn(x, step_size, format)
@@ -236,9 +314,17 @@ function function_max_error_special_inputs(
 
     f_format = Err.formats[format]
 
+    bigx = BigFloat(0.0)
+    bigz = BigFloat(0.0)
+    scratch = BigFloat(0.0)
+
+    fptr = mpfr_fptr(MPFR_UNARY[func])
+    apply_fn = (dest, x) -> apply!(dest, fptr, x)
+
     f = getfield(Base.Math, Symbol(func))
     for x in input_set
-        (error, y, z) = calculate_function(x, f, rounding, fastmath_on)
+        (error, y, z) = calculate_function(x, f, apply_fn, rounding,
+                                           fastmath_on, bigx, bigz, scratch)
         number_of_tests = number_of_tests + 1
         if (isnan(z))
             number_of_infs = number_of_infs + 1
@@ -246,10 +332,10 @@ function function_max_error_special_inputs(
 
         # Update max error and corresponding values.
         if error > max_error
-            max_error = error
+            max_error = deepcopy(error)
             max_input = x
             max_output = y
-            max_ref_out = z
+            max_ref_out = deepcopy(z)
         end
     end
 
